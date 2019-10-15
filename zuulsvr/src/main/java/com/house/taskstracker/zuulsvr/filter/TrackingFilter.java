0afacebf583dc3879a82365627efcb76534205c1
Mihai Crac <mihai.crac@arnia.ro>
2019-10-15 16:46:09 +0300

    add zuul, eureka and authentication

---
+++ zuulsvr/src/main/java/com/house/taskstracker/zuulsvr/filter/TrackingFilter.java
@@ -0,0 +1,56 @@
+package com.house.taskstracker.zuulsvr.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackingFilter extends ZuulFilter {
+    private static final int FILTER_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
+
+    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    @Autowired
+    private FilterUtils filterUtils;
+
+    @Override
+    public String filterType() {
+        return FilterUtils.PRE_FILTER_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FILTER_ORDER;
+    }
+
+    public boolean shouldFilter() {
+        return SHOULD_FILTER;
+    }
+
+    private boolean isCorrelationIdPresent() {
+        if (filterUtils.getCorrelationId() != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+
+    public Object run() {
+
+        if (isCorrelationIdPresent()) {
+            logger.debug("correlation-id found in tracking filter: {}. ", filterUtils.getCorrelationId());
+        } else {
+            filterUtils.setCorrelationId(generateCorrelationId());
+            logger.debug("correlation-id generated in tracking filter: {}.", filterUtils.getCorrelationId());
+        }
+
+        return null;
+    }
+}
